3938fc96ffa16cacedab1da07a0d9a8fabb8bebd
RyoCoder <89810037+RyoCoder@users.noreply.github.com>
2022-02-18 01:23:14 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/RyoCoder/MusicPlayerr /MusicPlayer
+git clone https://github.com/RyoCoder/MusicPlayerr /MusicPlayerr
 echo ">> INSTALLING REQUIREMENTS..."
-cd /MusicPlayer
+cd /MusicPlayerr
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
